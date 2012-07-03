ccff6fc3005a85463fc9f2ca3396b3550fff5d93
Kevin Esler <kevin.esler@gmail.com>
2012-07-03 13:49:38 -0400

    Adjusted the Scratch smoke test so it works.

--- src/test/scala/akka/amqp/Scratch.scala
+++ src/test/scala/akka/amqp/Scratch.scala
@@ -2,6 +2,7 @@ package akka.amqp
 
 import java.util.concurrent.CountDownLatch
 import akka.actor.{ ActorSystem, Actor, Props }
+import akka.serialization.SerializationExtension
 
 object Scratch extends App {
 
@@ -11,13 +12,17 @@ object Scratch extends App {
   val nrOfMessages = 2000
   val latch = new CountDownLatch(nrOfMessages)
 
+  val serialization = SerializationExtension(system)
+
   val deliveryHandler = connection.connectionProperties.system.actorOf(Props(new Actor {
     protected def receive = {
       case delivery @ Delivery(payload, routingKey, deliveryTag, isRedeliver, properties, sender) ⇒
-        println("D: " + new String(payload))
-
-        delivery.acknowledge()
-        latch.countDown()
+        serialization.deserialize(payload, classOf[String]) match {
+        case Left(t) => println("Error: deserialization failed")
+        case Right(ref) => println(ref.asInstanceOf[String])
+      }
+      delivery.acknowledge()
+      latch.countDown()
     }
   }))
   val consumer = connection.newConsumer(ActiveQueue("test"), deliveryHandler, false)
@@ -26,7 +31,9 @@ object Scratch extends App {
   val publisher = connection.newStashingPublisher(DefaultExchange)
 
   for (i ← 1 to nrOfMessages) {
-    publisher.publish(Message("Message[%s]".format(i).getBytes, "test"))
+    val msg = "Message[%s]".format(i)
+    val buf = msg.getBytes
+    publisher.publish(Message(msg, "test"))
   }
 
   latch.await()
