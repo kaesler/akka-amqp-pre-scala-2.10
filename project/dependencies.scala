5833b0aba7d634ed258c5319aa099cefedd1bac1
Kevin Esler <kevin.esler@gmail.com>
2012-11-02 13:30:12 -0400

    Upgraded some versions of depdendencies.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object dependencies {
 
-  def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.4"
-  def AkkaActor = "com.typesafe.akka" % "akka-actor" % "2.0.2"
-  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.8"
-  def JUnit = "junit" % "junit" % "4.7" % "test"
-  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit" % "2.0.2" % "test"
+  def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.7"
+  def AkkaActor = "com.typesafe.akka" % "akka-actor" % "2.0.3"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "2.0.M4"
+  def JUnit = "junit" % "junit" % "4.10" % "test"
+  def AkkaTestKit = "com.typesafe.akka" % "akka-testkit" % "2.0.3" % "test"
   def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
 }
