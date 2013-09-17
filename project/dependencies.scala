7356cf6f0eaf78a42341f42950674f6ce37c31a7
Kevin Esler <kevin.esler@gmail.com>
2013-09-17 14:57:01 -0400

    Updated Scalatest and sbt versions

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ object dependencies {
 
   def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.7"
   def AkkaActor = "com.typesafe.akka" % "akka-actor" % "2.0.3"
-  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "2.0.M4"
+  val scalatest = "org.scalatest" % "scalatest_2.9.1" % "2.0.M5b"
   def JUnit = "junit" % "junit" % "4.10" % "test"
   def AkkaTestKit = "com.typesafe.akka" % "akka-testkit" % "2.0.3" % "test"
   def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
